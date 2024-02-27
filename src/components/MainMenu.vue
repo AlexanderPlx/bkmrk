0469638b61b14bf6141499c19d46065fb6ddf53a
BorisSTLcorp <b.stalevskiy@encry.ru>
2024-02-27 16:43:28 +0300

    PFRPUV-78024: add limit of bookmarks and their`s shift

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -5,18 +5,45 @@ import { MenuOption } from 'naive-ui'
 type MainMenuOption = MenuOption & { url?: string }
 
 const createWinBox = useWinBox()
+const limitOfWinBox = 15;
+const windowWidth: number = window.innerWidth;
+const windowHeight: number = window.innerHeight;
+const widthWinBox = 900;
+const heightWinBox = 500;
+
+let counterWinBox = 0;
+let x = 280;
+let y = 0;
 
 const openRecord = (url?: string) => {
+
+  if (counterWinBox === limitOfWinBox) {
+  // necessary to add notification of achieved limitOfWinBox
+  return
+  }
+
   const randomId = Math.floor(Math.random() * 20) + 1;
 
   const winBox = createWinBox({
     title: url ?? `Fox #${randomId}`,
     url: url ?? `https://randomfox.ca/images/${randomId}.jpg`,
     class: 'modern',
-    x: '300px',
-    y: '20px'
+    x: `${x += 20}px`,
+    y: `${y += 20}px`,
+    width: `${widthWinBox}px`,
+    height: `${heightWinBox}px`,
+    onclose: () => {
+      --counterWinBox
+      return false
+    }
   })
 
+  if ((widthWinBox + x + 15) > windowWidth || (heightWinBox + y + 15) > windowHeight) {
+    x = 280;
+    y = 0;
+  }
+
+  ++counterWinBox;
 }
 
 const menuOptions: MainMenuOption[] & { url?: string } = [
@@ -91,6 +118,8 @@ const menuOptions: MainMenuOption[] & { url?: string } = [
     ]
   }
 ]
+
+const urlArray: string[] = [];
 
 const handleUpdateValue = (_key: string, item: MainMenuOption) => {
   const url = item.url
