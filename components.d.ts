eb0064e5a61ef3fc36259735f70fbbcee564b690
BorisSTLcorp <b.stalevskiy@encry.ru>
2024-03-27 14:03:41 +0300

    PFRPUV-78024: add ToolBar component

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,10 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    // Components
     MainMenu: typeof import('./src/components/MainMenu.vue')['default']
+    ToolBar: typeof import('./src/components/ToolBar.vue')['default']
+    // Tools
     NButton: typeof import('naive-ui')['NButton']
     NButtonGroup: typeof import('naive-ui')['NButtonGroup']
     NCard: typeof import('naive-ui')['NCard']
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { darkTheme } from 'naive-ui'
-import MainMenu from './components/MainMenu.vue'
 import { ref } from 'vue'
 
+import MainMenu from './components/MainMenu.vue'
+import ToolBar from './components/ToolBar.vue';
+
 const theme = ref(darkTheme);
-const showModalSettings = ref(false);
-const showModalLogin = ref(false);
 
 </script>
 
@@ -26,80 +26,10 @@ const showModalLogin = ref(false);
             <MainMenu />
           </n-message-provider>
 
-          <n-button-group class="button-group">
-            <n-button round
-                      @click="showModalLogin = true"
-            >
-              Login
-              <template>
-                <n-modal v-model:show="showModalLogin">
-                  <n-card
-                      style="width: 600px"
-                      title="LogIn/LogOut"
-                      :bordered="true"
-                      size="huge"
-                      role="dialog"
-                      aria-modal="true"
-                  >
-                    <form>
-                      <n-input
-                          type="text"
-                          placeholder="Login"
-                          :maxlength="12"
-                          style="margin-bottom: 10px"
-                      />
-                      <n-input
-                          type="password"
-                          show-password-on="mousedown"
-                          placeholder="Password"
-                          :maxlength="8"
-                          style="margin-bottom: 10px"
-                      />
-                    </form>
-                    <n-button-group>
-                      <n-button round
-                                @click="console.log('Login')">
-                        Login
-                      </n-button>
-                      <n-button round
-                                @click="console.log('Register')">
-                        Register
-                      </n-button>
-                    </n-button-group>
-                  </n-card>
-                </n-modal>
-              </template>
-            </n-button>
-            <n-button round
-                      @click="showModalSettings = true"
-            >
-              Settings
-              <template>
-                <n-modal v-model:show="showModalSettings">
-                  <n-card
-                      style="width: 600px"
-                      title="Settings"
-                      :bordered="true"
-                      size="huge"
-                      role="dialog"
-                      aria-modal="true"
-                  >
-                    <n-switch
-                        :default-value="!theme"
-                        @update:value="(v) => v ? theme = null : theme = darkTheme"
-                    >
-                      <template #checked>
-                        Light theme
-                      </template>
-                      <template #unchecked>
-                        Dark theme
-                      </template>
-                    </n-switch>
-                  </n-card>
-                </n-modal>
-              </template>
-            </n-button>
-          </n-button-group>
+          <ToolBar class="button-group"
+                   v-bind:theme="theme"
+                   @changeTheme="(value) => theme = value"
+          />
         </n-layout-sider>
         <n-layout>
         </n-layout>
